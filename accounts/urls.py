a5e0525e7cea4911d9c3cb28f30a05312a569f17
Soji <soji@Soji.local>
2021-02-08 23:54:09 +0900

    google login

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'accounts'
--- portfolio/settings.py
+++ portfolio/settings.py
@@ -153,7 +153,7 @@ AWS_SES_SECRET_ACCESS_KEY = env ('AWS_SES_SECRET_ACCESS_KEY')
 AWS_SWS_RESION_NAME = 'us-east-2'
 AWS_SES_RESION_ENDPOINT = 'email.us-east-2.amazonaws.com'
 EMAIL_BACKEND = 'django_ses.SESBackend'
-DEFAULT_FROM_EMAIL = 'appt39573@gmail.com'
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
 
 
 # メールサーバーへの接続設定
